3402dbe8d2fc308c2f7c792ea88f3eaa51c90006
idave1360 <102980889+idave1360@users.noreply.github.com>
2024-04-09 11:16:20 +0900

    Next.js 첫 실습

---
+++ 0409/hellonext/src/app/page.js
@@ -0,0 +1,72 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Hello Next.js
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            A <span>-&gt;</span>
+          </h2>
+          <p>A stands for apple.</p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            B <span>-&gt;</span>
+          </h2>
+          <p>B stands for banana.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            C <span>-&gt;</span>
+          </h2>
+          <p>C stands for cherry.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            D <span>-&gt;</span>
+          </h2>
+          <p>D stands for durian.</p>
+        </a>
+      </div>
+    </main>
+  );
+}
